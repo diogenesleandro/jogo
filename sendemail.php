a34736a2d490bb5e9a811f68dfae4447768d9f00
Diogenes Leandro <diogenes.pereira@etec.sp.gov.br>
2019-06-12 23:09:57 +0000

    div de jogador sendo clicada

--- sendemail.php
+++ sendemail.php
@@ -12,13 +12,21 @@
             $acerto = "SELECT * FROM acertos ORDER BY RAND() LIMIT 1";
             $qacerto = $mysqli->query($acerto);
             while ($dacerto = $qacerto->fetch_array()) {
-              echo $dacerto['ds_acertos'];
+                $resp['a']= $dacerto['ds_acertos'];
+                $resp['b']= "<h1 class='r'>CERTO</h1>";
+                echo $resp['b'];
+                echo $resp['a'];
             }
         }else{
             $erro = "SELECT * FROM prendas ORDER BY RAND() LIMIT 1";
             $qerro = $mysqli->query($erro);
             while ($derro = $qerro->fetch_array()) {
-              echo $derro['ds_prendas'];
+                $resp['a']= $derro['ds_prendas'];
+                $resp['b']= "<h1 class='r'>ERRADO</h1>";
+                echo $resp['b'];
+                echo $resp['a'];
+                
+             
             }
         }
     }
